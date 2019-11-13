8025729ce32c178ca11667ee8e215783e6ee383b
areddy108 <allstar.reddy@gmail.com>
2019-11-13 13:43:45 -0600

    Working Changes -Abhi

--- marc_part.js
+++ marc_part.js
@@ -4,8 +4,12 @@ function displayBox(i) {
 
     if (i == 0) { 
         curr.innerHTML = `<div id="title"> Monday </div> 60 minutes.`;
-    } else { 
-        curr.innerHTML = `<div id="title"> Under Development <div>`;
+    } else if (i==1) {
+        curr.innerHTML = `<div id="title"> Under Development <div>`
+    }else {
+        curr.innerHTML = `<div id="title"> Add Friend: Joe <div>`
+        var button = document.getElementById('button')
+        button.innerHTML = `<a href='friend.html'> Done </a>`
     }
 
     var show = document.getElementById("text-box") 
